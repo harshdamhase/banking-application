54747e5910c0bf64bdc58276bf714104470bced5
Harshda Mhase <harshadamhase12@gmail.com>
2023-10-19 06:44:42 -0400

    folder struture added

---
+++ src/Component/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Footer.css'
+import { Link } from 'react-router-dom';
+
+function Footer() {
+    return (
+        <div>
+            <footer className="footer">
+                <div className='container'>
+                    <p className='footer-title'>Developed By  @Harshda Mhase <a href="https://github.com/yogita-s-24/motiveminder"><i className="fa-brands fa-github github-icon"></i></a></p>
+                  
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
